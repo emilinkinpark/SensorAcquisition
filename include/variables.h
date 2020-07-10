b41c3beb0cd8bae2235261bb1ecaa2ba6a345207
emilinkinpark <emilinkinpark@gmail.com>
2020-07-10 17:02:24 +0600

    Production Mode

--- include/variables.h
+++ include/variables.h
@@ -2,24 +2,18 @@
 #include "DOpH.cpp"
 //#include "bme680.cpp"
 
-
-
 //Topic Declaration
-char tank_addr[8] = "TANK_1";           //Insert TANK Address
+char tank_addr[8] = "TANK_1"; //Insert TANK Address
 
-char HEARTBEAT_TOPIC[18] ;
+char HEARTBEAT_TOPIC[18];
 char DO_TOPIC[20];
 char pH_TOPIC[20];
 
 //WIFI
-#define SSID "GloryOffice"               //"Glory"                //"GloryAgro"
-#define PASS "razurahat@1!1"                                      //"Gloryart1!1"
+#define SSID "GloryAgro"   //"GloryOffice"//               //"Glory"                //
+#define PASS "Gloryart1!1" //"razurahat@1!1"//                                      //
 
 //MQTT
 //#define Client_Name "TANK1"
-#define MQTT_Broker_IP "192.168.1.9"//"192.168.0.29"//<- Raspberry Server
-#define MQTT_Fallback_IP "0.0.0.0" //Implementation Required
-
-
-
-
+#define MQTT_Broker_IP "192.168.0.29"//<- Raspberry Server "192.168.1.9" //
+#define MQTT_Fallback_IP "0.0.0.0"   //Implementation Required
