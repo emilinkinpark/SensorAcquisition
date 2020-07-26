db951d96a4eb4041a002185219664a71cde6af37
emilinkinpark <emilinkinpark@gmail.com>
2020-07-26 11:45:41 +0600

    Minor Changes

--- include/mqtt_variables.h
+++ include/mqtt_variables.h
@@ -6,12 +6,12 @@ TANK_3      192,168,0,16
 */
 
 //Topic Declaration12
-char tank_addr[8] = "TANK_1"; //Insert TANK Address
+char tank_addr[8] = "TANK_X"; //Insert TANK Address
 
 char HEARTBEAT_TOPIC[18];
 char DO_TOPIC[20];
 char pH_TOPIC[20];
 
 //MQTT
-#define MQTT_Broker_IP   "192.168.0.4" //<- AgroPC Broker    //"192.168.1.9" <- GloryOffice ////"192.168.0.2" //<- Raspberry Broker   "192.168.43.1"   // -< Redmi Broker
+#define MQTT_Broker_IP   "192.168.0.2" //<- AgroPC Broker    //"192.168.1.9" <- GloryOffice ////"192.168.0.2" //<- Raspberry Broker   "192.168.43.1"   // -< Redmi Broker
 #define MQTT_Fallback_IP "0.0.0.0"   //Implementation Required
